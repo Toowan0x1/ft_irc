e88de5b0ad1eca7d70e07b65476ee559375ab2b5
0xtoowan <omar.elhoumadi@gmail.com>
2024-03-09 02:23:55 +0100

    Mode 2

--- ft_irc/Commands/Mode.cpp
+++ ft_irc/Commands/Mode.cpp
@@ -33,8 +33,32 @@ static void    sendMsg(int fd, std::string msg)
     }
 }
 
+static int hasCharacter(const std::string& str, char ch) {
+    return (str.find(ch) != std::string::npos) ? 1 : 0;
+}
+
+static int	isValidMode(const std::string& mode, int clientFd)
+{
+	int i = 0;
+    int flag = 1;
+	while (mode[i])
+	{
+		if (mode[i] == 'i' || mode[i] == 'o' || mode[i] == 'w'
+				|| mode[i] == 'a' || mode[i] == 'r')
+		{
+            /* do nothing */
+        }
+		else {
+			std::string messageToSend = "";
+			sendMsg(clientFd, messageToSend);
+            flag = 0;
+			break;
+		}
+		i++;
+	}
+}
+
 void    Server::Mode(std::string line, int i) {
-    (void)i;
     (void)line;
     int args = countArguments(line);
     if (args > 1 && args < 4) {
@@ -78,15 +102,51 @@ void    Server::Mode(std::string line, int i) {
                 Client *client = *it;
                 if (client->_nickname == arg1)
                 {
+                    //if (_clientList[i]->_userMode)
+                    // check the mode if begin with - or + or one of the user modes iowar
+                    if (mode[0] == '-')
+                    {
+                        int j = 1;
+                        std::string tmp;
+                        // check mode str (improve this part later)
+                        if (isValidMode(mode, _clientList[i]->_clientFd))
+                        {
+                            while (mode[j])
+                            {
+                                if (mode[j] == 'i' || mode[j] == 'o' || mode[j] == 'w'
+                                    || mode[j] == 'a' || mode[j] == 'r')
+                                {
+                                    // -io
+                                    // wairo
+                                    int k = 0;
+                                    while (client->_userMode[k])
+                                    {
+                                        if (!client->_userMode[k] == mode[k])
+                                        {
+                                            tmp += client->_userMode[k];
+                                        }
+                                        k++;
+                                    }
+                                    client->_userMode = tmp;
+                                }
+                                j++;
+                            }
+                        }
+                    }
+                    else if (mode[0] == '+' || hasCharacter(mode, 'i') || hasCharacter(mode, 'o')
+                    || hasCharacter(mode, 'w') || hasCharacter(mode, 'a') || hasCharacter(mode, 'r'))
+                    {
+                        //
+                    }
+                    else
+                    {
+                        // invalid user mode to set
+                    }
                     flag = 1;
                     break;
                 }
             }
-            if (flag == 1)
-            {
-                //if (_clientList[i]->_userMode)
-            }
-            else
+            if (!flag)
             {
                 std::string messageToSend = "";
                 sendMsg(_clientList[i]->_clientFd, messageToSend);
@@ -100,7 +160,6 @@ void    Server::Mode(std::string line, int i) {
             for (it = _clientList.begin(); it != _clientList.end(); ++it) {
                 Client *client = *it;
                 if (client->_nickname == arg1) {
-                    //flag = 1;
                     std::string messageToSend = "MODE " + arg1 + " " + client->_userMode + "\n";
                     sendMsg(_clientList[i]->_clientFd, messageToSend);
                     flag = 1;
@@ -115,13 +174,9 @@ void    Server::Mode(std::string line, int i) {
         }
     }
 }
-
 /*
-
 MODE nickname +i    // Set invisible mode
 MODE nickname -a    // Unset away mode
 
 MODE nickname       // Query user modes
-MODE #channel       // Query channel modes
-
 */
